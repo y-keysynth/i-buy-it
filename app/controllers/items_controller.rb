9f4b1338a5311965baf9202347ea1b2fc94b9110
y-keysynth <ykhojojp@gmail.com>
2020-01-26 16:01:37 +0900

    user_idの取得を削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -69,6 +69,7 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:title, :details, :price, :user_id)
+      # params.require(:item).permit(:title, :details, :price, :user_id)
+      params.require(:item).permit(:title, :details, :price).merge(user_id: current_user.id)
     end
 end

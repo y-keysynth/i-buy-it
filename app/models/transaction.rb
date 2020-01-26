9f78415ffb4fad42fd28b945ca9c6b8b47f86725
y-keysynth <ykhojojp@gmail.com>
2020-01-26 15:20:46 +0900

    関連付け

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,9 @@
 class Transaction < ApplicationRecord
+  belongs_to :item
+  belongs_to :buyer,  class_name: 'User', foreign_key: 'buyer_id'
+  belongs_to :seller, class_name: 'User', foreign_key: 'seller_id', optional:true
+
+  # バリデーション
+  validates :item,  presence: true
+  validates :buyer, presence: true
 end

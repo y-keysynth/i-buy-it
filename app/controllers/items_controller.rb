639582d1663a1bed957b1fd336370432a9c03704
y-keysynth <ykhojojp@gmail.com>
2020-02-01 20:27:04 +0900

    mainのlistとりあえず完成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   # GET /items
   # GET /items.json
   def index
-    @items = Item.all
+    @items = Item.all.order(created_at: "DESC")
   end
 
   # GET /items/1

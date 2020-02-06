ee115b5d19f9a7b038d922b425cdcc3b194a1c48
y-keysynth <ykhojojp@gmail.com>
2020-02-06 14:31:58 +0900

    とりあえずpush。行き詰まり

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :liking_users, through: :likes, source: :user
+
+  validates :title, length: { in: 1..80 }
+
 end

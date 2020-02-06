dec2f79e47b53c61558fb5272247ea4d8cdde683
y-keysynth <ykhojojp@gmail.com>
2020-02-06 20:17:01 +0900

    userモデルバリデーションOK

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,7 @@ class Item < ApplicationRecord
   has_many :liking_users, through: :likes, source: :user
 
   validates :title, length: { in: 1..80 }
+  validates :price, length: { in: 1..11 }
+  validates :details, presence: true
 
 end

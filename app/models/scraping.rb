dac84e487c03a1e13a5afcad2f6854683af3033a
y-keysynth <ykhojojp@gmail.com>
2020-02-13 21:29:01 +0900

    自動デプロイ

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -7,12 +7,14 @@ class Scraping
     # url = gets.chomp
     # page = agent.get(url)
     page = agent.get("https://jp.finalfantasyxiv.com/lodestone/character/28815431/blog/4345570/")
-    blog_comment = page.search('.entry__blog__box__area h2')
-    puts "コメント：#{blog_comment.inner_text}"
+    blog_sentence = page.search('.txt_selfintroduction')
+    puts "内容：#{blog_sentence.inner_text}"
     chara_name = page.search('.frame__chara__name')
     puts "ユーザー名：#{chara_name.inner_text}"
     server_name = page.search('.frame__chara__world')
     puts "サーバー名：#{server_name.inner_text}"
+
+    
     end
 end
 

27a48c61fc153aecf8c2603cda9d0e835f890649
y-keysynth <ykhojojp@gmail.com>
2020-02-13 20:30:51 +0900

    singsupcontrolerをコメントアウト

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,27 +1,27 @@
-class SignupsController < ApplicationController
-  has_secure_password validations: true
+# class SignupsController < ApplicationController
+#   has_secure_password validations: true
 
-  validates :comment, presence: true
-  validates :url, presence: true
-  validates :mail, presence: true, uniqueness: true
+#   validates :comment, presence: true
+#   validates :url, presence: true
+#   validates :mail, presence: true, uniqueness: true
 
-  def new
-    @user = User.new
-  end
+#   def new
+#     @user = User.new
+#   end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to login_path
-    else
-      render 'new'
-    end
-  end
+#   def create
+#     @user = User.new(user_params)
+#     if @user.save
+#       redirect_to login_path
+#     else
+#       render 'new'
+#     end
+#   end
 
-  private
+#   private
 
-    def user_params
-      params.require(:user).permit(:name, :mail, :password, :password_confirmation)
-    end
+#     def user_params
+#       params.require(:user).permit(:name, :mail, :password, :password_confirmation)
+#     end
 
-end
+# end

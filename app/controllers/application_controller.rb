50af056ef11a8e94a8803625ffc0f07721360b82
y-keysynth <ykhojojp@gmail.com>
2020-02-15 17:38:46 +0900

    本番環境のみ適応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_search
-  before_action :basic_auth
+  before_action :basic_auth, if: :production? 
   protect_from_forgery with: :exception
 
 def set_search
@@ -16,6 +16,10 @@ end
 
 
   private
+
+  def production?
+    Rails.env.production?
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|

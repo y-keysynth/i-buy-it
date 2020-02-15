a32317e72c5e17d8c92f32777e3a1232c93e28c7
y-keysynth <ykhojojp@gmail.com>
2020-02-15 18:00:18 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == 'admin' && password == '2222'
     end
   end
 

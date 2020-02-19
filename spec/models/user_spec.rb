8443c64c929e6b6e787625b388c87862aee03efc
y-keysynth <ykhojojp@gmail.com>
2020-02-19 21:04:39 +0900

    userのテストLGTM

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,67 +8,54 @@ describe User do
       expect(user).to be_valid
     end
 
-    # # 2. nameが空では登録できないこと
-    # it "is invalid without a name" do
-    #   user = build(:user, name: nil)
-    #   user.valid?
-    #   expect(user.errors[:name]).to include("can't be blank")
-    # end
+    # 2. nameが空では登録できないこと
+    it "is invalid without a name" do
+      user = build(:user, name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include("を入力してください")
+    end
 
-    # # 3. emailが空では登録できないこと
-    # it "is invalid without a email" do
-    #   user = build(:user, email: nil)
-    #   user.valid?
-    #   expect(user.errors[:email]).to include("can't be blank")
-    # end
+    # 3. emailが空では登録できないこと
+    it "is invalid without a email" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("が入力されていません。")
+    end
 
-    # # 4. passwordが空では登録できないこと
-    # it "is invalid without a password" do
-    #   user = build(:user, password: nil)
-    #   user.valid?
-    #   expect(user.errors[:password]).to include("can't be blank")
-    # end
+    # 4. passwordが空では登録できないこと
+    it "is invalid without a password" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("が入力されていません。")
+    end
 
-    # # 5. passwordが存在してもpassword_confirmationが空では登録できないこと
-    # it "is invalid without a password_confirmation although with a password" do
-    #   user = build(:user, password_confirmation: "")
-    #   user.valid?
-    #   expect(user.errors[:password_confirmation]).to include("doesn't match Password")
-    # end
+    # 5. passwordが存在してもpassword_confirmationが空では登録できないこと
+    it "is invalid without a password_confirmation although with a password" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
+    end
 
-    # # 6. nameが7文字以上であれば登録できないこと
-    # it "is invalid with a name that has more than 7 characters " do
-    #   user = build(:user, name: "aaaaaaaa")
-    #   user.valid?
-    #   expect(user.errors[:name]).to include("is too long (maximum is 6 characters)")
-    # end
+    # 6. 重複したemailが存在する場合登録できないこと
+    it "is invalid with a duplicate email address" do
+      user = create(:user)
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("は既に使用されています。")
+    end
 
-    # # 7. nameが6文字以下では登録できること
-    # it "is valid with a name that has less than 6 characters " do
-    #   user = build(:user, name: "aaaaaa")
-    #   expect(user).to be_valid
-    # end
+    # 7. passwordが6文字以上であれば登録できること
+    it "is valid with a password that has more than 6 characters " do
+      user = build(:user, password: "000000", password_confirmation: "000000")
+      user.valid?
+      expect(user).to be_valid
+    end
 
-    # # 8. 重複したemailが存在する場合登録できないこと
-    # it "is invalid with a duplicate email address" do
-    #   user = create(:user)
-    #   another_user = build(:user, email: user.email)
-    #   another_user.valid?
-    #   expect(another_user.errors[:email]).to include("has already been taken")
-    # end
-
-    # # 9. passwordが6文字以上であれば登録できること
-    # it "is valid with a password that has more than 6 characters " do
-    #   user = build(:user, password: "000000", password_confirmation: "000000")
-    #   user.valid?
-    #   expect(user).to be_valid
-    # end
-
-    # # 10. passwordが5文字以下であれば登録できないこと
-    # it "is invalid with a password that has less than 5 characters " do
-    #   user = build(:user, password: "00000", password_confirmation: "00000")
-    #   user.valid?
-    #   expect(user.errors[:password]).to include("is too short (minimum is 6 characters)")
-    # end
+    # 8. passwordが5文字以下であれば登録できないこと
+    it "is invalid with a password that has less than 5 characters " do
+      user = build(:user, password: "00000", password_confirmation: "00000")
+      user.valid?
+      expect(user.errors[:password]).to include("は6文字以上に設定して下さい。")
+    end
   end
 end

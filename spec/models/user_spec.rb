73505c64010f66c638bfd81b4fa85b0676a4999b
y-keysynth <ykhojojp@gmail.com>
2020-02-19 21:36:02 +0900

    userのテスト調整LGTM

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,41 +2,46 @@ require 'rails_helper'
 
 describe User do
   describe '#create' do
-    # 1. nameとemail、passwordとpassword_confirmationが存在すれば登録できること
+    # nameとemail、passwordとpassword_confirmationが存在すれば登録できること
+    # 運用用「urlとemailとランダムパスワード、passwordとpassword_confirmationが存在すれば登録できること」に変更
     it "is valid with a name, email, password, password_confirmation" do
       user = build(:user)
       expect(user).to be_valid
     end
 
-    # 2. nameが空では登録できないこと
+    # nameが空では登録できないこと
+    # 運用用「urlが空では登録できないこと」に変更
     it "is invalid without a name" do
       user = build(:user, name: nil)
       user.valid?
       expect(user.errors[:name]).to include("を入力してください")
     end
 
-    # 3. emailが空では登録できないこと
+    # 運用用「ランダムパスワードが空では登録できないこと」
+    # 運用用「urlが空では登録できないこと」
+
+    # emailが空では登録できないこと
     it "is invalid without a email" do
       user = build(:user, email: nil)
       user.valid?
       expect(user.errors[:email]).to include("が入力されていません。")
     end
 
-    # 4. passwordが空では登録できないこと
+    # passwordが空では登録できないこと
     it "is invalid without a password" do
       user = build(:user, password: nil)
       user.valid?
       expect(user.errors[:password]).to include("が入力されていません。")
     end
 
-    # 5. passwordが存在してもpassword_confirmationが空では登録できないこと
+    # passwordが存在してもpassword_confirmationが空では登録できないこと
     it "is invalid without a password_confirmation although with a password" do
       user = build(:user, password_confirmation: "")
       user.valid?
       expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
     end
 
-    # 6. 重複したemailが存在する場合登録できないこと
+    # 重複したemailが存在する場合登録できないこと
     it "is invalid with a duplicate email address" do
       user = create(:user)
       another_user = build(:user, email: user.email)
@@ -44,18 +49,28 @@ describe User do
       expect(another_user.errors[:email]).to include("は既に使用されています。")
     end
 
-    # 7. passwordが6文字以上であれば登録できること
+    # passwordが6文字以上であれば登録できること
     it "is valid with a password that has more than 6 characters " do
       user = build(:user, password: "000000", password_confirmation: "000000")
       user.valid?
       expect(user).to be_valid
     end
 
-    # 8. passwordが5文字以下であれば登録できないこと
+    # passwordが5文字以下であれば登録できないこと
     it "is invalid with a password that has less than 5 characters " do
       user = build(:user, password: "00000", password_confirmation: "00000")
       user.valid?
       expect(user.errors[:password]).to include("は6文字以上に設定して下さい。")
     end
+
+    # 重複した名前が存在する場合登録できないこと
+    # 運用用「重複したurlが存在する場合登録できないこと」に変更
+    it "is invalid with a duplicate name address" do
+      user = create(:user)
+      another_user = build(:user, name: user.name)
+      another_user.valid?
+      expect(another_user.errors[:name]).to include("はすでに存在します")
+    end
+
   end
 end

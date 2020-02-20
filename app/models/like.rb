8dde2505acdc26db478032681d587fd6a22166de
y-keysynth <ykhojojp@gmail.com>
2020-02-20 13:31:53 +0900

    likeのモデルテストLGTM

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :item, counter_cache: :likes_count
   belongs_to :user
+
+  validates_uniqueness_of :item_id, scope: :user_id
+  validates :item_id, presence: true
+  validates :user_id, presence: true
 end
